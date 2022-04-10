dbab1e51d4865840ee67a75fab6604aca339e6fc
Ryan DeLong <rdelong111@gmail.com>
2022-04-10 13:21:06 -0500

    commit tree 0109302801829e6a138ae35ed09d1e1742758c73

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
-function App() {
-  const [items, setItems] = useState(itemData);
-  const [isDarkMode, setIsDarkMode] = useState(false);
+class App extends React.Component {
+  state = {
+    items: itemData,
+    isDarkMode: false
+  };
 
-  function handleDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
+  handleDarkMode = () => {
+    this.setState(prevState => {
+      return {isDarkMode: !prevState.isDarkMode};
+    });
   }
 
-  return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={handleDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
-      <ShoppingList items={items} />
+  render() {
+    return (
+      <div className={"App " + (this.state.isDarkMode ? "dark" : "light")}>
+        <header>
+          <h2>Shopster</h2>
+          <button onClick={this.handleDarkMode}>
+            {this.state.isDarkMode ? "Dark" : "Light"} Mode
+          </button>
+        </header>
+        <ShoppingList items={this.state.items} />
     </div>
-  );
+    )
+  }
 }
 
 export default App;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,38 +1,44 @@
-import React, { useState } from "react";
+import React from "react";
+import items from "../data/items";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
+class ShoppingList extends React.Component {
+  state = {
+    selectedCategory: 'All'
+  };
 
-  function handleCategoryChange(event) {
-    // event.target.value will be the value selected by the user
-    setSelectedCategory(event.target.value);
+  handleCategoryChange = (e) => {
+    this.setState({selectedCategory: e.target.value});
   }
 
-  // we want to filter the items to only display the ones based on the selected category
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  displayItems = () => {
+    return (
+      items.filter((item) => {
+        if (this.state.selectedCategory === 'All') return true;
+        return item.category === this.state.selectedCategory;
+      }).map((item) => {
+        return <Item key={item.id} name={item.name} category={item.category} />
+      })
+    )
+  }
 
-    return item.category === selectedCategory;
-  });
-
-  return (
-    <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
+  render() {
+    return (
+      <div className="ShoppingList">
+        <div className="Filter">
+          <select name="filter" onChange={this.handleCategoryChange}>
+            <option value="All">Filter by category</option>
+            <option value="Produce">Produce</option>
+            <option value="Dairy">Dairy</option>
+            <option value="Dessert">Dessert</option>
+          </select>
+        </div>
+        <ul className="Items">
+          {this.displayItems()}
+        </ul>
       </div>
-      <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
-      </ul>
-    </div>
-  );
+    );
+  }
 }
 
 export default ShoppingList;
